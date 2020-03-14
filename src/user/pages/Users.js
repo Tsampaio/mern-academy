471a30ed33bd43a948e65f036efd3eb6f5dd948a
Telmo Sampaio <telmoasampaio@gmail.com>
2020-03-14 10:46:48 +0000

    Adding the UserListPage/Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const User = () => {
-    return (
-        <h2>Users Works!</h2>
-    )
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Telmo Sampaio',
+            image: 'https://avatars0.githubusercontent.com/u/13276002?s=460&u=a7fa449f48e44ae8214102b181c57a7dcfdc3299&v=4',
+            places: 3
+        }
+    ];
+    return <UsersList items={USERS}/>;
 }
 
 export default User;
