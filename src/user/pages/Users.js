7e1db08f484bbca8009b8e9a8c88a3ef926482c1
Telmo Sampaio <telmoasampaio@gmail.com>
2020-03-14 18:00:50 +0000

    styling our App

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,12 +3,12 @@ import UsersList from '../components/UsersList';
 
 const User = () => {
     const USERS = [
-        // {
-        //     id: 'u1',
-        //     name: 'Telmo Sampaio',
-        //     image: 'https://avatars0.githubusercontent.com/u/13276002?s=460&u=a7fa449f48e44ae8214102b181c57a7dcfdc3299&v=4',
-        //     places: 3
-        // }
+        {
+            id: 'u1',
+            name: 'Telmo Sampaio',
+            image: 'https://avatars0.githubusercontent.com/u/13276002?s=460&u=a7fa449f48e44ae8214102b181c57a7dcfdc3299&v=4',
+            places: 3
+        }
     ];
     return <UsersList items={USERS}/>;
 }
